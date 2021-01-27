9d980dfae9c1410e175825e93b67c58bcc8cbae3
mayafrog <29991258+mayafrog@users.noreply.github.com>
2021-01-27 19:59:53 +1030

    68 ms

--- solutions/209. Minimum Size Subarray Sum.py
+++ solutions/209. Minimum Size Subarray Sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
     
-        minWindowSize = len(nums)+1
-        currentWindowSum = 0
-        windowStart = 0
+        window = len(nums)+1
+        sum = 0
+        l = 0
 ​
-        for windowEnd in range(len(nums)):
-            currentWindowSum += nums[windowEnd]
+        for r in range(0,len(nums)):
+            sum += nums[r]
 ​
-            while currentWindowSum >= s:
-                minWindowSize = min(minWindowSize, windowEnd-windowStart + 1)
-                currentWindowSum -= nums[windowStart]
-                windowStart += 1
+            while sum >= s:
+                window = min(window, r-l + 1)
+                sum -= nums[l]
+                l += 1
 ​
-        return minWindowSize if minWindowSize <= len(nums) else 0
+        return window if window <= len(nums) else 0
