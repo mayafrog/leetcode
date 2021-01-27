0f77686fc1e995206ceb95b324932b0c3f1cada8
mayafrog <29991258+mayafrog@users.noreply.github.com>
2021-01-28 01:30:35 +1030

    260 ms

---
+++ solutions/628. Maximum Product of Three Numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+​
+        a = nums[0] * nums[1] * nums[2]
+        b = nums[0] * nums[-1] * nums[-2]
+​
+        return max(a, b)
