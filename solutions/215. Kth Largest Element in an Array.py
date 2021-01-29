e790c9452a8cbacce3801c0c3ed896c2411be55d
mayafrog <29991258+mayafrog@users.noreply.github.com>
2021-01-29 21:11:01 +1030

    60 ms

--- solutions/215. Kth Largest Element in an Array.py
+++ solutions/215. Kth Largest Element in an Array.py
@@ -11,11 +11,11 @@ class Solution:
         left = [x for x in nums if x < pivot]
         print ("left:", left)
 ​
-        L, M = len(right), len(mid)
+        R, M = len(right), len(mid)
 ​
-        if k <= L:
+        if k <= R:
             return self.findKthLargest(right, k)
-        elif k > L + M:
-            return self.findKthLargest(left, k - L - M)
+        elif k > R + M:
+            return self.findKthLargest(left, k - R - M)
         else:
             return mid[0]
