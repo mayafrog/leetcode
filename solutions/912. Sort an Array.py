94fc43f4cba0fa97f9b2d894e6685361a8d3d71c
mayafrog <29991258+mayafrog@users.noreply.github.com>
2021-02-15 21:45:38 +1030

    312 ms

--- solutions/912. Sort an Array.py
+++ solutions/912. Sort an Array.py
@@ -1,4 +1,12 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        return nums
+        if (len(nums) <= 1):
+            return nums
+        
+        pivot = random.choice(nums)
+​
+        left = [i for i in nums if i < pivot]
+        middle = [i for i in nums if i == pivot]
+        right = [i for i in nums if i > pivot]
+​
+        return self.sortArray(left) + middle + self.sortArray(right)
